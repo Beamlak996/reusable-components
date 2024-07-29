bc1dfd1766c82fe2b782ebcf397ec2256299c3b4
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-07-29 05:46:26 -0900

    sidebar with submenu done

--- src/layout/root-layout.tsx
+++ src/layout/root-layout.tsx
@@ -2,11 +2,9 @@ import { Sidebar } from "@/components/reusable/sidebar/sidebar"
 
 export const RootLayout = ({children}: {children: React.ReactNode}) => {
     return (
-        <div className="flex gap-5" >
-            <Sidebar />
-            <main className="max-w-5xl flex-1 mx-auto py-4 bg-[#f1f1f1] min-h-screen" >
-                {children}
-            </main>
-        </div>
-    )
+      <div className="flex gap-5">
+        <Sidebar />
+        <main className="flex-1 py-4">{children}</main>
+      </div>
+    );
 }
