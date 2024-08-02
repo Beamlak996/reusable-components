28d283f754220b8cebc7ea2755120d7294b24fb8
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-08-02 06:52:06 -0900

    shadcn form test with build in input done

--- src/pages/storage.tsx
+++ src/pages/storage.tsx
@@ -1,10 +1,53 @@
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
+const TestForm = z.object({
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});
 
 export const Stroage = () => {
+  const form = useForm<z.infer<typeof TestForm>>({
+    resolver: zodResolver(TestForm),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof TestForm>) => {
+    console.log(values)
+  };
+
   return (
     <div>
-        Storage
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            name="name"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <input {...field} placeholder="name" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <button type="submit" >Click</button>
+        </form>
+      </Form>
     </div>
-  )
+  );
 };
-
