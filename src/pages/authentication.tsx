e3ab58db4683fee7a0c25ae13282f1b2b2b50f67
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-08-14 02:32:05 -0900

    tanstack table

--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -1,13 +1,5 @@
-import { useGetAllProductsQuery, useGetProductQuery } from "@/api/api-slice";
 
 export const Authentication = () => {
-    const { data: products, isError, isLoading } = useGetAllProductsQuery({});
-    const { data:product } = useGetProductQuery("iphone")
-
-    console.log(products)
-    console.log(product)
-
-    if(isLoading) return <div>Loading...</div>
 
     return (
         <div>
